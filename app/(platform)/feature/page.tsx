95c72f6a2248ba22ce4e138c5c5ba6d636686c95
abdellatif ahmed <abdoahmed6130@gmail.com>
2026-04-18 10:15:29 +0200

    refactor: move app system section into a separate component

--- app/(platform)/feature/page.tsx
+++ app/(platform)/feature/page.tsx
@@ -1,55 +1,11 @@
+import AppSystemSection from "@/shared/components/Pages/Feature/AppSystemSection";
 import FeatureHeroSection from "@/shared/components/Pages/Feature/FeatureHeroSection";
-import Image from "next/image";
 
 const Page = () => {
   return (
     <>
       <FeatureHeroSection />
-      <section className="flex flex-col items-center m-4">
-        <div className="feature-headline">
-          نظام التطبيق
-          <span></span>
-        </div>
-        <h2 className="text-3xl text-center w-70 font-semibold mb-10 md:text-5xl md:w-150 md:leading-15 ">
-          ما هي منصة
-          <span className="text-(--tenant-primary)"> سابق ؟</span>
-        </h2>
-        <div className="flex flex-col lg:flex-row gap-8 items-stretch">
-          <div className="flex flex-col justify-between py-2 lg:w-1/2">
-            <p className="text-base font-medium leading-6 md:text-lg lg:leading-7">
-              منصة <span className="text-(--tenant-primary)">سابق</span> هي منصة
-              رقمية متكاملة صُممت خصيصًا لمساعدة المدارس على إدارة العمليات
-              التعليمية والإدارية من خلال بيئة موحدة و سهلة الاستخدام. تجمع
-              المنصة بين مختلف الأدوات التي تحتاجها المدرسة في مكان واحد، مما
-              يقلل من الاعتماد على الأنظمة المتفرقة ويُسهم في تنظيم العمل بشكل
-              أكثر كفاءة.
-            </p>
-
-            <p className="text-base font-medium leading-6 md:text-lg lg:leading-7">
-              تتيح <span className="text-(--tenant-primary)">سابق</span>{" "}
-              للإدارات التعليمية متابعة بيانات الطلاب، تنظيم الجداول الدراسية، و
-              إدارة الحضور و الاختبارات بطريقة أكثر سلاسة و دقة، مع
-              توفير رؤية واضحة تساعد على تحسين سير العمل اليومي و
-              اتخاذ قرارات أفضل.
-            </p>
-
-            <p className="text-base font-medium leading-6 md:text-lg lg:leading-7">
-              كما تساهم في تقليل الوقت و الجهد المبذول في المهام
-              الإدارية الروتينية، مما يمنح فريق العمل فرصة أكبر للتركيز على تطوير العملية التعليمية.
-            </p>
-          </div>
-
-          <figure className="w-full lg:w-1/2">
-            <Image
-              src="/images/app-system.svg"
-              alt="app-system image"
-              width={600}
-              height={400}
-              className="w-full h-full object-contain"
-            />
-          </figure>
-        </div>
-      </section>
+      <AppSystemSection/>
     </>
   );
 };
