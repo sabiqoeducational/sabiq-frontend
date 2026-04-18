2bc86fd85f3a7a066b1808512438ab98641da0b9
abdellatif ahmed <abdoahmed6130@gmail.com>
2026-04-18 08:51:49 +0200

    Create Feature Page and Add Hero Section

---
+++ app/(platform)/feature/page.tsx
@@ -0,0 +1,11 @@
+import FeatureHeroSection from "@/shared/components/Pages/Feature/FeatureHeroSection";
+
+const Page = () => {
+  return (
+    <>
+    <FeatureHeroSection />
+    </>
+  );
+};
+
+export default Page;
